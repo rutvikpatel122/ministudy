048fdb9b92643f98b19a0a0fc2b47e817f1def15
rutvikpatel122 <pr3179217@gmail.com>
2024-10-04 18:56:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Webview Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const WebViewPage(
+          url: 'http://api.ministudy.in'), // Replace with your desired URL
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  final String url;
+  const WebViewPage({super.key, required this.url});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView Example'),
+      ),
+      body: WebViewPage(
+        url: widget.url,
+      ),
+    );
+  }
+}
